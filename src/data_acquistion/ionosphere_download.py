a60b4c1bf21a6e156787c34a8d3d13dfc1478cff
Zach Hoppinen <Zachhoppinen@gmail.com>
2025-07-11 14:21:43 -0600

    fixing ionosphere

--- src/data_acquistion/ionosphere_download.py
+++ src/data_acquistion/ionosphere_download.py
@@ -3,6 +3,11 @@ from urllib.parse import urljoin
 import shutil
 import os
 import subprocess
+import numpy as np
+import pandas as pd
+import xarray as xr
+from tqdm import tqdm
+import re
 
 def get_ionex_filename(year, day, old_format, HHMM = '0000', TYP = 'FIN', TSMP = '02H', CNT = 'GIM', cente = 'esa', zipped = True):
     """
@@ -30,7 +35,7 @@ def download_ionex(year, day, output_dir, center = 'esa'):
     if isinstance(output_dir, str): output_dir = Path(output_dir)
     if Path(output_dir.joinpath(get_ionex_filename(year, day, old_format = False, zipped = False))).exists(): return
 
-    old_format = True if year <= 2022 and day <= 330 else False
+    old_format = True if year < 2022 or (year == 2022 and  day <= 330) else False
     
     main_https = 'https://cddis.nasa.gov/archive/gnss/products/ionex/'
     filename = get_ionex_filename(year, day, old_format)
@@ -45,11 +50,6 @@ def download_ionex(year, day, output_dir, center = 'esa'):
     
     return output_dir.joinpath(get_ionex_filename(year, day, old_format = False, zipped = False))
 
-for year in range(2016, 2026):
-    print(year)
-    for day in range(1, 366):
-        download_ionex(year, day, output_dir = '/Users/rdcrlzh1/Documents/SWE_error_analysis/local/ionosphere')
-
 def parse_map(tecmap, exponent = -1):
     tecmap = re.split('.*END OF TEC MAP', tecmap)[0]
     return np.stack([np.fromstring(l, sep=' ') for l in re.split('.*LAT/LON1/LON2/DLON/H\\n',tecmap)[1:]])*10**exponent
@@ -59,7 +59,16 @@ def get_tecmaps(filename):
         ionex = f.read()
         return [parse_map(t) for t in ionex.split('START OF TEC MAP')[1:]]
 
+for year in range(2016, 2026):
+    print(year)
+    for day in range(329, 366):
+        download_ionex(year, day, output_dir = '/Users/rdcrlzh1/Documents/SWE_error_analysis/local/ionosphere')
+
+
 ixs = sorted(list(Path('/Users/rdcrlzh1/Documents/SWE_error_analysis/local/ionosphere').glob('*.INX')))
+
+xs = np.arange(-180.0, 185.0, 5.0)
+ys = np.arange(87.5, -89, -2.5)
 
 ion = np.zeros((len(ys), len(xs), len((ixs))))
 times = []
@@ -69,9 +78,19 @@ for i, fp in enumerate(tqdm(ixs)):
     # 6 is 12 UTC or ~5-7am MT
     ion[:, :, i] = get_tecmaps(fp)[6]
 
-xs = np.arange(-180.0, 185.0, 5.0)
-ys = np.arange(87.5, -89, -2.5)
 
 ion = xr.DataArray(ion, coords = {'y': ys, 'x': xs, 'time': times})
 output_dir = Path('/Users/rdcrlzh1/Documents/SWE_error_analysis/local/ionosphere')
 ion.to_netcdf(output_dir.joinpath('am_ion.nc'))
+
+ion = np.zeros((len(ys), len(xs), len((ixs))))
+times = []
+for i, fp in enumerate(tqdm(ixs)):
+    time = pd.to_datetime(fp.stem.split('_')[1], format = '%Y%j0000')
+    times.append(time)
+    # 11 is 24 UTC or ~5-7pm MT
+    ion[:, :, i] = get_tecmaps(fp)[11]
+
+ion = xr.DataArray(ion, coords = {'y': ys, 'x': xs, 'time': times})
+output_dir = Path('/Users/rdcrlzh1/Documents/SWE_error_analysis/local/ionosphere')
+ion.to_netcdf(output_dir.joinpath('pm_ion.nc'))
